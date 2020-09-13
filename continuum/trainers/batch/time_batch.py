840128e728b56cfa94799809337db38cd27581b2
Kevin Hill <kivo360@gmail.com>
2020-09-13 00:11:48 -0400

    Beginning to run tests

--- continuum/trainers/batch/time_batch.py
+++ continuum/trainers/batch/time_batch.py
@@ -1,6 +1,14 @@
+import pandas as pd
+from loguru import logger
+from continuum.data.loaders import times
+
+
 class TimeBatchTrainer:
     """Time Batch Learning
-    Instead of taking one sample we take many samples then learn a lot quickly.
+    Instead of taking one sample we take many samples then learn batches of them for time series.
     """
-    def __init__(self) -> None:
-        pass
+    def __init__(self, frame: pd.DataFrame) -> None:
+        self.times_frame = times.TimeseriesDataset(frame)
+
+    def train(self):
+        logger.debug("Training on test data")
